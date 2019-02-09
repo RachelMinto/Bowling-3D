ed9c04e5f82432f273b1a88f546dfee50dd6301a
Rachel Minto <rminto@wistia.com>
2019-02-09 12:40:23 -0500

    Initialize bowling ball with SFX.

---
+++ Assets/Scripts/BowlingBall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingBall : MonoBehaviour {
+    [SerializeField] AudioSource rollingSFX;
+
+	void Start () {
+        Vector3 initialVelocity = new Vector3(0, 0, -350);
+        GetComponent<Rigidbody>().velocity = initialVelocity;
+	}
+}
